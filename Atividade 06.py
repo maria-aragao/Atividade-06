e786e1e73ba7e750ae31be606c9f264133cb5e2f
maria-aragao <mariafloraragao1@gmail.com>
2024-09-09 10:15:20 -0300

    Atividade de juntar nome com sobrenome

---
+++ Atividade 06.py
@@ -0,0 +1,6 @@
+# Crie um programa que receba dois nomes (primeiro nome e sobrenome) e exiba o nome completo.
+
+primeironome = str(input("Digite o primeiro nome"))
+sobrenome = str(input("Digite o segundo nome"))
+
+print(f"seu nome é {primeironome} {sobrenome}")
